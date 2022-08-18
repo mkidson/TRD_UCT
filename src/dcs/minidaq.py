f896751837c7e4adee9b590a6ca5eaca1ac7811a
NNathanson <nathansonnina@gmail.com>
2022-08-18 15:29:50 +0200

    start modifying minidaq

--- src/dcs/minidaq.py
+++ src/dcs/minidaq.py
@@ -1,4 +1,9 @@
 
+from datetime import datetime
+import os
+from re import T
+import signal
+import time
 import click
 import zmq
 
@@ -24,12 +29,57 @@ def minidaq(ctx):
 
 
 @minidaq.command()
+@click.option('--n_events','-n', default=5, help='Number of triggered events you want to read.')
+@click.pass_context
+
+def trigger_read(ctx, n_events):
+    # scopeReader = scopeRead.Reader("ttyACM3")
+    #run_period = time.time() + 60*0.5 #How long you want to search for triggers for
+    # trig_count_1 = int(os.popen('trdbox reg-read 0x102').read().split('\n')[0])
+
+    ctx.obj.trdbox.send_string("read 0x102")
+    trig_count_1 = int(ctx.obj.trdbox.recv_string(), 16)
+    print(trig_count_1)
+    # os.system("trdbox unblock")
+    trig_count_2 = 0
+    ctx.obj.trdbox.send_string("write 0x103 1")
+    i = 0
+    
+    while i < (n_events):
+        ctx.obj.trdbox.send_string("read 0x102")
+        trig_count_2 = int(ctx.obj.trdbox.recv_string(), 16)
+        print(trig_count_2)
+
+        if trig_count_2 != trig_count_1:
+            i += 1
+
+            try:
+                 ctx.invoke(readevent)
+            except:
+                 i -= 1
+
+            ctx.obj.trdbox.send_string("read 0x102")
+            trig_count_1 = int(ctx.obj.trdbox.recv_string(), 16)
+            ctx.obj.trdbox.send_string("write 0x103 1")
+        else:
+            pass
+
+
+
+
+@minidaq.command()
 @click.pass_context
 def readevent(ctx):
 
-    ctx.obj.trdbox.send_string(f"write 0x08 1") # send trigger
+    # ctx.obj.trdbox.send_string(f"write 0x103 1") # unblocks
+    # ctx.obj.trdbox.send_string(f"write 0x08 1") # send trigger
     print(ctx.obj.trdbox.recv_string())
 
     ctx.obj.sfp0.send_string("read")
     data = ctx.obj.sfp0.recv()
     print(len(data))
+
+    f = open("data", "wb")
+    f.write(data)
+    f.close()
+
